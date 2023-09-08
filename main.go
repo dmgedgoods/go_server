bf0ce702f299dce630405f060c36b27336af6afd
dmgedgoods <smcdani8@mail.ccsf.edu>
2023-09-08 13:41:42 -0700

    Go server works

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
     address := r.FormValue("address")
     fmt.Fprintf(w, "Name = %s\n", name)
     fmt.Fprintf(w, "Address = %s\n", address)
+}
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 if r.URL.Path != "/hello" {
